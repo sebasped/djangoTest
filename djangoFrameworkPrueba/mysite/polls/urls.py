5121e42d4ef1e8e3c271712a50fcd0a785f8fdd3
Santiago <sebasped@gmail.com>
2019-05-06 15:43:12 -0300

    backend question id

--- djangoFrameworkPrueba/mysite/polls/urls.py
+++ djangoFrameworkPrueba/mysite/polls/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # path('questions/', views.get_questions, name='questions'), # el name es para los templates
     path('questions/', views.get_questions),
+    path('question/<int:question_id>/', views.get_question_by_id),
 ]
--- djangoFrameworkPrueba/mysite/polls/views.py
+++ djangoFrameworkPrueba/mysite/polls/views.py
@@ -1,25 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 from .models import Question
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.core import serializers
+from django.forms.models import model_to_dict
 
-# def index(request):
-#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     latest_question_list = Question.objects.order_by('-pub_date')
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
 
 def get_questions(request):
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     listaSQL = Question.objects.all().values().order_by('-pub_date')
     lista = list(listaSQL) # convierto el Query SQL a una lista para poder pasarlo a json
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    # return HttpResponse("Hello, world. You're at the polls index.")
     return JsonResponse(lista, safe=False)
+
+def get_question_by_id(request,question_id):
+    obj = Question.objects.get(pk=question_id)
+    dict_obj = model_to_dict( obj )
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return JsonResponse(dict_obj, safe=False)
